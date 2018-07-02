456af79fc7f1c7a4a69b2bff14d8466a59c1a4bb
André Luiz R S Andrade <a.luiz.souto@gmail.com>
2018-07-02 17:02:10 -0300

    Mudança na tela de login adionada uma mensagem

--- src/app/segurado/login/login.component.ts
+++ src/app/segurado/login/login.component.ts
@@ -5,4 +5,12 @@ import { Component } from "@angular/core";
     templateUrl: 'login.component.html',
     styleUrls: ['login.component.css']
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+    login(event){
+
+        event.preventDefault();
+
+    }
+
+}
