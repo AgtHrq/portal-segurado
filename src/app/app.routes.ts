da14fa691535860811039d6863274e4028266067
André Luiz R S Andrade <a.luiz.souto@gmail.com>
2018-07-10 14:33:11 -0300

    Adicionada rota da esqueci a senha

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { LogadoComponent } from './logado/logado.component';
 import { Routes, RouterModule } from "@angular/router";
 import { LoginComponent } from "./segurado/login/login.component";
+import { ForgotPasswordComponent } from './segurado/forgot-password/forgot-password.component';
 
 const APP_ROUTES: Routes = [
 
     { path: "", component: LoginComponent },
-    { path: "logado", component: LogadoComponent},
+    { path: "logado", component: LogadoComponent },
+    { path: "esqueci/senha", component: ForgotPasswordComponent },
     { path: "**", redirectTo: "" }
 
 ];
