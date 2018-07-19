52379c31e2c2c09fe99791203611ae903ad6e76c
André Luiz R S Andrade <a.luiz.souto@gmail.com>
2018-07-19 14:22:11 -0300

    Criacao do service de troca de senha.

--- src/app/segurado/forgot-password/change-password/change-password.component.ts
+++ src/app/segurado/forgot-password/change-password/change-password.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ForgotPasswordService } from '../../../services/forgot-password/forgot-password.service';
 import { upperCase, lowerCase, containNumber, equal } from '../../../validators/password.validator';
+import { Router } from '../../../../../node_modules/@angular/router';
 
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.component.html',
   styleUrls: ['./change-password.component.css']
 })
-export class ChangePasswordComponent implements OnInit {
+export class ChangePasswordComponent implements OnInit, OnChanges {
 
   @Input() hash: string = "";
   formNewPassword: FormGroup;
   classMin: string = "error";
   classLetraMin: string = "error";
   classLetraMai: string = "error";
+  showSuccessMessage: boolean = false;
 
   constructor(private formBuilder: FormBuilder, private forgotService: ForgotPasswordService) {
 
@@ -25,13 +27,35 @@ export class ChangePasswordComponent implements OnInit {
           Validators.required, Validators.minLength(6), Validators.maxLength(14), upperCase, lowerCase, containNumber
         ]
       ],
-      confirmPassword: ""
+      confirmPassword: ["", Validators.required]
     });
 
     this.formNewPassword.setValidators([ equal ]);
 
    }
 
+   novaSenha(event, data){
+
+    event.preventDefault();
+
+    delete data.confirmPassword;
+
+    console.log(data);
+
+    this.forgotService.novaSenha(data).subscribe(
+      data => {
+        this.showSuccessMessage = true;
+      }
+    );
+
+   }
+
   ngOnInit() { }
+
+  ngOnChanges(){
+
+    this.formNewPassword.get("hash").setValue(this.hash);
+
+  }
 
 }
--- src/app/segurado/forgot-password/first-verification/first-verification.component.ts
+++ src/app/segurado/forgot-password/first-verification/first-verification.component.ts
@@ -1,7 +1,7 @@
 import { MaskUtils } from '../../../utils/mask-utils';
 import { Pergunta } from '../../../models/pergunta';
 import { PerguntaService } from '../../../services/perguntas/pergunta.service';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { ForgotPasswordService } from '../../../services/forgot-password/forgot-password.service';
 
@@ -10,7 +10,7 @@ import { ForgotPasswordService } from '../../../services/forgot-password/forgot-
   templateUrl: './first-verification.component.html',
   styleUrls: ['./first-verification.component.css']
 })
-export class FirstVerificationComponent implements OnInit {
+export class FirstVerificationComponent implements OnInit, OnChanges {
 
   @Input() cpf: string = "";
   util: MaskUtils = new MaskUtils();
@@ -60,5 +60,11 @@ export class FirstVerificationComponent implements OnInit {
     }
 
   ngOnInit() { }
+
+  ngOnChanges() {
+
+    this.formChange.get("cpf").setValue(this.cpf);
+
+  }
 
 }
