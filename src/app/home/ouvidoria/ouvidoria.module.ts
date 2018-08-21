fd2c7ded81d2e2e97892f4266bc6f27fdc1e0d6a
André Luiz R S Andrade <a.luiz.souto@gmail.com>
2018-08-21 14:13:24 -0300

    Adicinado o RouterModule no modulo de ouvidoria

--- src/app/home/ouvidoria/ouvidoria.module.ts
+++ src/app/home/ouvidoria/ouvidoria.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+
 import { OuvidoriaComponent } from './ouvidoria.component';
 import { RespotaComponent } from './respota/respota.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   declarations: [
     OuvidoriaComponent, 
