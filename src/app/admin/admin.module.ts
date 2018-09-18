ad4b9b5d4d60a2938eb6a59b82a27df8e37ad25f
André Luiz R S Andrade <a.luiz.souto@gmail.com>
2018-09-18 14:51:05 -0300

    Criacao do atributo descricao no component de admin-submenu.

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,8 +5,6 @@ import { RouterModule } from '@angular/router';
 import { AdminComponent } from './admin.component';
 import { AdminMenuComponent } from './admin-menu/admin-menu.component';
 import { AdminSubmenuComponent } from './admin-submenu/admin-submenu.component';
-import { AdminHomeComponent } from './admin-home/admin-home.component';
-import { CadastroUsuarioComponent } from './admin-home/cadastro-usuario/cadastro-usuario.component';
 import { HomeModule } from '../home/home.module';
 import { AdminOuvidoriaModule } from './admin-ouvidoria/admin-ouvidoria.module';
 import { AdminHomeModule } from './admin-home/admin-home.module';
@@ -23,8 +21,7 @@ import { AdminSubmenuModule } from './admin-submenu/admin-submenu.module';
   ],
   declarations: [
     AdminComponent, 
-    AdminMenuComponent, 
-    CadastroUsuarioComponent
+    AdminMenuComponent
   ],
   exports: [
     AdminSubmenuComponent
