4ae7cc1aaec6ec22ab395a0629cff2ddb99dfdb3
André Luiz R S Andrade <a.luiz.souto@gmail.com>
2018-10-26 12:00:59 -0300

    Finalização de notificação de usuario.

--- src/app/home/notificacao/notificacao.component.ts
+++ src/app/home/notificacao/notificacao.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit} from '@angular/core';
 
 import { HomeUtils } from './../../utils/home-utils';
+import { NotificacaoService } from 'src/app/services/notificacao/notificacao.service';
 
 @Component({
   selector: 'app-notificacao',
@@ -9,16 +10,20 @@ import { HomeUtils } from './../../utils/home-utils';
 })
 export class NotificacaoComponent implements OnInit {
 
-  notificacoes = ["1", "2", "3"];
+  notificacoes: any = [];
   numNotificacoes: number = 0;
   
-  constructor(private utils: HomeUtils) { }
+  constructor(private utils: HomeUtils, private notificacaoService: NotificacaoService) { }
 
   ngOnInit() {
 
-    this.numNotificacoes = this.notificacoes.length;
-    this.utils.notificacoes();
-
+    
+    this.notificacaoService.getNotificacoesSegurado().subscribe(n => {
+      this.notificacoes = n.json()
+      this.numNotificacoes = this.notificacoes.length;
+      this.utils.notificacoes();
+    }
+      );
 
   }
   
