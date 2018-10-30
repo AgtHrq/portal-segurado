41047f17103f55bad646e484d81cfe693dab1705
André Luiz R S Andrade <a.luiz.souto@gmail.com>
2018-10-30 15:26:12 -0300

    Adicionando ordenacao na lista de solicitacoes em home/solicitacao/list-solicitacao/list-solicitacao.compnent.ts

--- src/app/home/solicitacao/list-solicitacao/list-solicitacao.component.ts
+++ src/app/home/solicitacao/list-solicitacao/list-solicitacao.component.ts
@@ -60,6 +60,7 @@ export class ListSolicitacaoComponent implements OnInit, OnChanges {
     if(this.solicitacoes != null){
 
       this.solicitacoes.sort((a, b) => a.dataCriacao > b.dataCriacao ? -1 : 1);
+      this.solicitacoes.sort((a, b) => a.status === true ? -1 : 1);
 
     }
 
