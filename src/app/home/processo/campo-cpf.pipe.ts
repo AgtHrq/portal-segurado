c55b7a788de0ba6cc7ade3a6f2bd4de5d4eb0f5a
André Luiz R S Andrade <a.luiz.souto@gmail.com>
2018-12-12 16:44:47 -0300

    Remocao de linha em home/processo/campo-pipe

--- src/app/home/processo/campo-cpf.pipe.ts
+++ src/app/home/processo/campo-cpf.pipe.ts
@@ -15,7 +15,6 @@ export class MaskCampoPipe implements PipeTransform {
     campo = campo.trim();
     return tipoCampo.toUpperCase() == 'CPF' ? `${campo.substring(0, 3)}.${campo.substring(3, 6)}.${campo.substring(6, 9)}-${campo.substring(9, 11)}` : 
       tipoCampo.toUpperCase() == 'MATRICULA' ? `${campo.substr(0, 3)}.${campo.substr(3, 3)}-${campo.substr(6)}` : campo;
-
   }
 
 }
